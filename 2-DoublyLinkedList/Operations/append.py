b1e3a294a7d1f362093ed7b1c4be9f23de3e02f8
ayyushipatel2509 <ayup25092@gmail.com>
2026-07-18 13:58:22 -0400

    renamed

---
+++ 2-DoublyLinkedList/Operations/append.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+        self.prev = None
+    
+class DoublyLinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+    def print(self):
+        current = self.head
+        while current is not None:
+            print(current.value, end="->")
+            current = current.next
+        print("None")
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        return True
+
+
+
+my_linked_list = DoublyLinkedList(7)
+my_linked_list.print()
+my_linked_list.append(3)
+my_linked_list.append(9)
+my_linked_list.print()
+
+
+
+        
