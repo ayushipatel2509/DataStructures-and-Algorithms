85fec2adbb78bac6bbe405852846f78a8ccfc98b
ayyushipatel2509 <ayup25092@gmail.com>
2026-07-31 08:42:42 -0400

    Squares of sorted Array

--- 3-Arrays/squaresOfSortedArray.py
+++ 3-Arrays/squaresOfSortedArray.py
@@ -1,15 +1,19 @@
 def square_of_sorted_array(arr):
-    new_arr = []
+    new_arr = [0] * len(arr)
+    pos = len(new_arr) -1 
     left = 0 
     right = len(arr) -1
     while left <=  right :
         square_left = arr[left] * arr[left]
         square_right = arr[right] *arr[right]
         if square_left < square_right:
-            new_arr.append(square_left)
+            new_arr[pos] = square_right
+            right = right -1
         else:
-            new_arr.append(square_right)
-        left = left +1
-        right = right -1
-
+            new_arr[pos] = square_left
+            left = left +1
+        pos = pos -1
     return new_arr
+
+
+print(square_of_sorted_array([-2,-1,0,3,4]))
